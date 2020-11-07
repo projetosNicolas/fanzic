f16b28bdc17d56f8eaa162656a314da1fb8ee7fd
NicolasLira <71357959+projetosNicolas@users.noreply.github.com>
2020-11-06 21:08:18 -0300

    Update e1-2.js

--- script/e1-2.js
+++ script/e1-2.js
@@ -7,7 +7,7 @@ let taJanela = window.innerWidth;
 function mostra(conteudo) {
     botao.style.visibility = 'hidden';
     final.style.visibility = 'visible';
-    conteudo.innerHTML = "<p>Para nos previnir dessa doença, temos que</p><h1>FICAR EM CASA</h1><h1>LAVAR AS MÃOS CONSTANTIMENTE COM AGUA E SABÃO E USAR ÁLCOOL EM GEL</h1><h1>SE SAIR MATER UMA DISTÂNCIA DE 2 METROS DAS PESSOAS E USAR MÁSCARA O TEMPO TODO</h1>";
+    conteudo.innerHTML = "<p>Para nos previnir dessa doença, temos que</p><h1>FICAR EM CASA</h1><h1>LAVAR AS MÃOS CONSTANTIMENTE COM AGUA E SABÃO E USAR ÁLCOOL EM GEL</h1><h1>SE SAIR MANTER DISTÂNCIA DE 2 METROS DAS PESSOAS E USAR MÁSCARA O TEMPO TODO</h1>";
     if (taJanela < 600) {
     aluno.style.width = 30 + '%';
     prof.style.width = 20 + '%';
